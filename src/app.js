f502a0428733aed967a1e033f40f2cc9ead30641
ximet <dq.ximet@gmail.com>
2017-07-27 18:37:56 +0300

    update app configuration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import AppView from './views/AppView.js';
+
 import configureStore from './store/configureStore.js';
 const store = configureStore();
 
---
+++ src/views/AppView.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { GoogleMap, Marker } from 'react-google-maps';
+import { GOOGLE_API_KEY } from '../../config.js';
+
+const googleMapURL = `https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry&key=${GOOGLE_API_KEY}`
+
+
+class AppView export React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+
+    )
+  }
+}
