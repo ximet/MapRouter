ee1399a4f072756864b3e4c4984e40c5b097a118
ximet <dq.ximet@gmail.com>
2017-07-27 18:41:40 +0300

    add babelrc and scripts

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -5,14 +5,19 @@ import { GOOGLE_API_KEY } from '../../config.js';
 const googleMapURL = `https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry&key=${GOOGLE_API_KEY}`
 
 
-class AppView export React.Component {
+class AppView extends React.Component {
   constructor (props) {
     super(props);
   }
 
   render () {
     return (
-
+      <GoogleMap
+        defaultZoom={3}
+        defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+        googleMapURL={googleMapURL}
+      >
+      </GoogleMap>
     )
   }
 }
