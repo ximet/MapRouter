5d8c4c81813df8eeef8f0b48b32ab01d26c2234c
ximet <dq.ximet@gmail.com>
2017-07-30 13:39:28 +0300

    update page

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -1,28 +1,43 @@
 import React from 'react';
-import { GoogleMap, Marker } from 'react-google-maps';
+import { GoogleMap, Marker, withGoogleMap } from 'react-google-maps';
 import { GOOGLE_API_KEY } from '../../config.js';
 
 const googleMapURL = `https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry&key=${GOOGLE_API_KEY}`
 
+const GettingStartedGoogleMap = withGoogleMap(props => (
+  <GoogleMap ref = { props.onMapLoad }
+    defaultZoom = { 3 }
+    defaultCenter = {
+        { lat: -25.363882, lng: 131.044922 }
+    }
+    googleMapURL = { googleMapURL } >
+    </GoogleMap>
+));
+
 
 class AppView extends React.Component {
-  constructor (props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render () {
-    return (
-      <div>
-        OLOLO
-      <GoogleMap
-        defaultZoom={3}
-        defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
-        googleMapURL={googleMapURL}
-      >
-      </GoogleMap>
-      </div>
-    )
-  }
+    render() {
+        return (
+          <div >
+            OLOLO
+            <GettingStartedGoogleMap
+              containerElement={
+                <div style={{ height: `100%` }} />
+              }
+              mapElement={
+                <div style={{ height: `100%` }} />
+              }
+              onMapLoad={()=>{}}
+              onMapClick={()=>{}}
+              onMarkerRightClick={()=>{}}
+            />
+          </div>
+        )
+    }
 }
 
 export default AppView;
