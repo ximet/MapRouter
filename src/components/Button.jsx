cc95b314efbb0fd0cb683837a45d783ed591419f
ximet <dq.ximet@gmail.com>
2017-07-30 18:30:50 +0300

    add prop-types to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import RaisedButton from 'material-ui/RaisedButton';
 
-export const Button = () => (
-  <RaisedButton label="Default" />
+export const Button = (props) => (
+  <RaisedButton 
+    label={props.label} 
+    onTouchTap={props.onTouchTap}
+    disabled={props.disabled}
+  />
 );
+
+
+Button.propTypes = {
+    label: PropTypes.string,
+    onTouchTap: PropTypes.func,
+    disabled: PropTypes.bool,
+};
+
+Button.defaultProps = {
+    onTouchTap: () => {},
+    label: 'Default',
+    disabled: false
+};
