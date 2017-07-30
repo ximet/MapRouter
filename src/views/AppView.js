e6c1621dd465b29995e6d24e1b24cbe463c15917
ximet <dq.ximet@gmail.com>
2017-07-30 18:04:37 +0300

    update solution: delete connect googlemap

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -2,18 +2,15 @@ import React from 'react';
 import { GoogleMap, Marker, withGoogleMap } from 'react-google-maps';
 import { GOOGLE_API_KEY } from '../../config.js';
 
-const googleMapURL = `https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry&key=${GOOGLE_API_KEY}`
-
 const GettingStartedGoogleMap = withGoogleMap(props => (
   <GoogleMap ref = { props.onMapLoad }
     defaultZoom = { 3 }
     defaultCenter = {
         { lat: -25.363882, lng: 131.044922 }
     }
-    googleMapURL = { googleMapURL } >
+   >
     </GoogleMap>
 ));
-
 
 class AppView extends React.Component {
     constructor(props) {
@@ -22,7 +19,7 @@ class AppView extends React.Component {
 
     render() {
         return (
-          <div >
+          <div style={{  height: `100%`, flex: 1 }}>
             OLOLO
             <GettingStartedGoogleMap
               containerElement={
