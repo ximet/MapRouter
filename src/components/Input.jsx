c6aa1779fd0a97bb0fc63302b37ff6672cfebfa4
ximet <dq.ximet@gmail.com>
2017-07-30 18:27:55 +0300

    add prop-types

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TextField from 'material-ui/TextField';
 
-export const Input = () => (
-  <TextField label="Default" />
+export const Input = (props) => (
+  <TextField 
+    errorText={props.errorText}
+    onChange={props.onChange}
+    value={props.value}
+    type={props.type}
+    disabled={props.disabled}
+   />
 );
+
+Input.propTypes = {
+    errorText: PropTypes.string,
+    onChange: PropTypes.func,
+    value: PropTypes.string,
+    type: PropTypes.string,
+    disabled: PropTypes.bool,
+};
+
+Input.defaultProps = {
+    onChange: () => {},
+    errorText: '',
+    value: '',
+    type: 'text',
+    disabled: false
+};
